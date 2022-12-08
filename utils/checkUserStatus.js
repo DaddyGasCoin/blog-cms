9fe145476dd19b5f66af96bb3eac5e5542af63c2
Michael Jones <iammichaeljon27@gmail.com>
2022-12-08 09:26:32 +0400

    Code clean up

--- utils/checkUserStatus.js
+++ utils/checkUserStatus.js
@@ -1,27 +1,4 @@
 
-
-// export default function checkUserStatus() {
-
-//   let token = localStorage.getItem('token_info')
-//   if (!token) {
-//     token = sessionStorage.getItem('token_info')
-//   }
-//   if (token) {
-//     //If token exists;check if expired
-//     //JWT expires after 24 hours
-//     //If expired clear from storage;user must login again so return false
-//     token = JSON.parse(token)
-//     if (parseInt(token.expiration) > Math.floor(new Date().getTime() / 1000.0))
-//       return token
-//     else {
-//       //Token expired
-//       localStorage.clear()
-//       return false
-//     }
-//   }
-//   // return token
-// }
-
 export default function checkUserStatus(token) {
   if (token) {
     // If token exists, check if expired
